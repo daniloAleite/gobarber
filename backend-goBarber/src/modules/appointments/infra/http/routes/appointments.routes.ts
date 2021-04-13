7cbe351451fb4fe38f60e597d76abde7761dd00d
Danilo Almeida Leite <danilo.a.leite@gmail.com>
2021-04-13 19:43:06 -0300

    *

--- backend-goBarber/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend-goBarber/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticaded';
 import AppointmentsController from '../controllers/AppointmentsController';
@@ -10,7 +11,16 @@ const providerAppointmentsController = new ProviderAppointmentsController();
 
 appoitmentsRouter.use(ensureAthenticated);
 
-appoitmentsRouter.post('/', appointmentsController.create);
+appoitmentsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      provider_id: Joi.string().uuid().required(),
+      date: Joi.date(),
+    },
+  }),
+  appointmentsController.create,
+);
 appoitmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appoitmentsRouter;
