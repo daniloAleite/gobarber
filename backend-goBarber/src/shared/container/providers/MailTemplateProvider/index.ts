3f14a6b2f03d9954fc81932161aa9e4c4b427989
Danilo Almeida Leite <danilo.a.leite@gmail.com>
2021-06-28 21:16:18 -0300

    alterações

---
+++ backend-goBarber/src/shared/container/providers/MailTemplateProvider/index.ts
@@ -0,0 +1,13 @@
+import { container } from 'tsyringe';
+
+import IMailTemplateprovider from './models/IMailTemplateprovider';
+import HandlebarsMailTemplateProvider from './implementations/HandlebarsMailTemplateProvider';
+
+const providers = {
+  handlebars: HandlebarsMailTemplateProvider,
+};
+
+container.registerSingleton<IMailTemplateprovider>(
+  'MailTemplateProvider',
+  providers.handlebars,
+);
